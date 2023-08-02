43ea8bac916a072698eb667cd1de01244403331f
Sharvil Nichat <104116728+sharvil112003@users.noreply.github.com>
2023-08-02 21:51:02 +0530

    Add files via upload

---
+++ sensors_code.ino
@@ -0,0 +1,89 @@
+#include <Wire.h>
+#include "MAX30105.h"
+
+#include "heartRate.h"
+
+const int GSR =A0;
+int sensorValue=0;
+int gsr_average=0;
+
+MAX30105 particleSensor;
+
+const byte RATE_SIZE = 4; //Increase this for more averaging. 4 is good.
+byte rates[RATE_SIZE]; //Array of heart rates
+byte rateSpot = 0;
+long lastBeat = 0; //Time at which the last beat occurred
+
+float beatsPerMinute;
+int beatAvg;
+
+void setup() {
+  Serial.begin(9600);
+//  Serial.begin(9600);
+  Serial.println("Initializing...");
+
+  // Initialize sensor
+  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
+  {
+    Serial.println("MAX30105 was not found. Please check wiring/power. ");
+    while (1);
+  }
+  Serial.println("Place your index finger on the sensor with steady pressure.");
+
+  particleSensor.setup(); //Configure sensor with default settings
+  particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
+  particleSensor.setPulseAmplitudeGreen(0);
+}
+
+void loop() {
+  long irValue = particleSensor.getIR();
+  long sum=0;
+
+  if (checkForBeat(irValue) == true)
+  {
+    //We sensed a beat!
+    long delta = millis() - lastBeat;
+    lastBeat = millis();
+
+    beatsPerMinute = 60 / (delta / 1000.0);
+
+    if (beatsPerMinute < 255 && beatsPerMinute > 20)
+    {
+      rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
+      rateSpot %= RATE_SIZE; //Wrap variable
+
+      //Take average of readings
+      beatAvg = 0;
+      for (byte x = 0 ; x < RATE_SIZE ; x++)
+        beatAvg += rates[x];
+      beatAvg /= RATE_SIZE;
+    }  
+  
+
+    for(int i=0;i<10;i++){
+    sensorValue=analogRead(GSR);
+    sum+=sensorValue;
+    delay(50);
+    }
+    delay(10);
+  }
+  Serial.print("IR=");
+  Serial.print(irValue);
+  Serial.print(", BPM=");
+  Serial.print(beatsPerMinute);
+  Serial.print(", Avg BPM=");
+  Serial.print(beatAvg);
+
+  if (irValue < 50000)
+    Serial.print(" No finger?");
+
+  Serial.println();  
+  
+
+  gsr_average=sum/10;
+  Serial.print("GSR_Average =");
+  Serial.println(gsr_average);
+//  int human_resistance =((1024+2*gsr_average)*10000)/(512-gsr_average);
+//  Serial.print("Human resistance=");
+//  Serial.println(human_resistance); 
+}
